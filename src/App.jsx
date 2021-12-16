1d3690953a2ca40a8d902225c8fe3a3d0b3bf5e1
Thomas Kirst <thomas@moneygarden.de>
2021-12-16 12:09:38 +0100

    Outsourced product form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,158 +1,29 @@
-import './App.css';
-import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import TextInput from './components/TextInput';
-import NumberInput from './components/NumberInput';
-import Checkbox from './components/Checkbox';
-import Select from './components/Select';
-import RadioButton from './components/RadioButton';
+import styled from 'styled-components';
 
-import isProductValid from './lib/validation';
+import ProductForm from './components/ProductForm';
+
+import './App.css';
+
 import { saveToLocal, loadFromLocal } from './lib/localStorage';
 
 function App() {
-  const initialProduct = {
-    name: '',
-    price: 0,
-    isDecorated: false,
-    category: '',
-    packageSize: '',
-    contactEmail: '',
-    tags: [],
-  };
-  const [product, setProduct] = useState(initialProduct);
-
   const localStorageProducts = loadFromLocal('_products');
   const [products, setProducts] = useState(localStorageProducts ?? []);
-
-  const [hasFormErrors, setHasFormErrors] = useState(false);
 
   useEffect(() => {
     saveToLocal('_products', products);
   }, [products]);
 
-  const categories = [
-    'Tee',
-    'Lebkuchen',
-    'Kekse',
-    'Adventskalender',
-    'Liköre & Spirituosen',
-  ];
-
-  const handleChange = (event) => {
-    let inputValue = event.target.value; // "Glühwein"
-
-    if (event.target.type === 'checkbox') {
-      inputValue = event.target.checked;
-    }
-
-    // if (event.target.name === 'price') { parseInt }
-
-    setProduct({
-      // alle bestehenden Properties behalten
-      // neu zu setzende Property -> deren Wert überschreiben
-      ...product,
-      [event.target.name]: inputValue,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (isProductValid(product)) {
-      setProducts([...products, product]);
-      // setProduct(initialProduct);
-      setHasFormErrors(false);
-    } else {
-      setHasFormErrors(true);
-    }
-  };
+  const addProduct = (product) => setProducts([...products, product]);
 
   return (
     <Container>
-      <section>
-        <h2>🎄 Add a new product 🍪</h2>
-        {hasFormErrors && (
-          <ErrorMessage>
-            <div>
-              🎅🏽
-              <div className="bubble">🗯</div>
-            </div>
-            <p>
-              <strong>Ho ho ho! </strong>
-              Please check if all fields are correctly filled.
-            </p>
-          </ErrorMessage>
-        )}
-        <Form onSubmit={handleSubmit}>
-          <TextInput
-            onTextInputChange={handleChange}
-            name="name"
-            value={product.name}
-          >
-            Product Name
-          </TextInput>
-
-          <InputRow>
-            <div>
-              <NumberInput
-                name="price"
-                value={product.price}
-                onNumberInputChange={handleChange}
-              >
-                Price (in €)
-              </NumberInput>
-            </div>
-
-            <Checkbox
-              name="isDecorated"
-              value={product.isDecorated}
-              onCheckboxChange={handleChange}
-            >
-              decorated
-            </Checkbox>
-          </InputRow>
-
-          <Select
-            name="category"
-            value={product.category}
-            options={categories}
-            onSelectChange={handleChange}
-          >
-            Product Category
-          </Select>
-
-          <RadioButton value={product.packageSize} onRadioChange={handleChange}>
-            Package Size
-          </RadioButton>
-
-          <TextInput
-            name="contactEmail"
-            value={product.contactEmail}
-            onTextInputChange={handleChange}
-            placeholder="Add your email …"
-          >
-            Contact Email
-          </TextInput>
-
-          <div>
-            <button>Add Product</button>
-            {/* Optional */}
-            <button
-              type="reset"
-              onClick={() => {
-                setProduct(initialProduct);
-                setHasFormErrors(false);
-              }}
-            >
-              Reset
-            </button>
-          </div>
-        </Form>
-      </section>
-
+      <ProductForm onAddProduct={addProduct} />
       <CardTree>
         {products.map((product, index) => (
           <article
+            key={index}
             className={'area' + (index < 10 ? index + 1 : '')}
             style={
               index > 9 ? { gridRowStart: Math.floor((index - 2) / 4) + 3 } : {}
@@ -179,73 +50,6 @@ const Container = styled.div`
   height: 100%;
   margin: 0 auto;
   width: 80%;
-`;
-
-const InputRow = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Form = styled.form`
-  background: #620202;
-  padding: 0.7rem 0.5rem 1.2rem;
-  border-radius: 10px;
-
-  label {
-    display: block;
-    font-weight: bold;
-  }
-  input,
-  select {
-    padding: 0.25rem;
-    margin: 0.5rem 0 1rem;
-  }
-
-  button {
-    background: var(--button-bg);
-    border: 2px solid var(--button-bg);
-    border-radius: 5px;
-    color: var(--primary-color);
-    font-size: 1.2rem;
-    font-weight: 600;
-    padding: 0.5rem;
-    width: 49%;
-  }
-  button:first-child {
-    margin-right: 2%;
-  }
-  button:nth-child(even) {
-    background: transparent;
-  }
-`;
-
-const ErrorMessage = styled.div`
-  align-items: center;
-  background: var(--warning);
-  border-radius: 6px;
-  color: white;
-  display: flex;
-  gap: 2.5rem;
-  margin: 0 0 1rem;
-  padding: 0.5rem;
-
-  div {
-    font-size: 2.5rem;
-    display: inline-block;
-    position: relative;
-    transition: all 0.5s;
-  }
-  div:hover {
-    transform: rotateZ(20deg);
-  }
-
-  div.bubble {
-    font-size: 3rem;
-
-    position: absolute;
-    top: -17px;
-    right: -38px;
-  }
 `;
 
 const CardTree = styled.div`
---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,211 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import TextInput from './TextInput';
+import NumberInput from './NumberInput';
+import Checkbox from './Checkbox';
+import Select from './Select';
+import RadioButton from './RadioButton';
+
+import isProductValid from '../lib/validation';
+
+export default function ProductForm({ onAddProduct }) {
+  const initialProduct = {
+    name: '',
+    price: 0,
+    isDecorated: false,
+    category: '',
+    packageSize: '',
+    contactEmail: '',
+    tags: [],
+  };
+
+  const [product, setProduct] = useState(initialProduct);
+  const [hasFormErrors, setHasFormErrors] = useState(false);
+
+  const categories = [
+    'Tee',
+    'Lebkuchen',
+    'Kekse',
+    'Adventskalender',
+    'Liköre & Spirituosen',
+  ];
+
+  const handleChange = (event) => {
+    let inputValue = event.target.value; // "Glühwein"
+
+    if (event.target.type === 'checkbox') {
+      inputValue = event.target.checked;
+    }
+
+    // if (event.target.name === 'price') { parseInt }
+
+    setProduct({
+      // alle bestehenden Properties behalten
+      // neu zu setzende Property -> deren Wert überschreiben
+      ...product,
+      [event.target.name]: inputValue,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isProductValid(product)) {
+      onAddProduct(product);
+      setProduct(initialProduct);
+      setHasFormErrors(false);
+    } else {
+      setHasFormErrors(true);
+    }
+  };
+
+  return (
+    <section>
+      <h2>🎄 Add a new product 🍪</h2>
+      {hasFormErrors && (
+        <ErrorMessage>
+          <div>
+            🎅🏽
+            <div className="bubble">🗯</div>
+          </div>
+          <p>
+            <strong>Ho ho ho! </strong>
+            Please check if all fields are correctly filled.
+          </p>
+        </ErrorMessage>
+      )}
+      <Form onSubmit={handleSubmit}>
+        <TextInput
+          onTextInputChange={handleChange}
+          name="name"
+          value={product.name}
+        >
+          Product Name
+        </TextInput>
+
+        <InputRow>
+          <div>
+            <NumberInput
+              name="price"
+              value={product.price}
+              onNumberInputChange={handleChange}
+            >
+              Price (in €)
+            </NumberInput>
+          </div>
+
+          <Checkbox
+            name="isDecorated"
+            value={product.isDecorated}
+            onCheckboxChange={handleChange}
+          >
+            decorated
+          </Checkbox>
+        </InputRow>
+
+        <Select
+          name="category"
+          value={product.category}
+          options={categories}
+          onSelectChange={handleChange}
+        >
+          Product Category
+        </Select>
+
+        <RadioButton value={product.packageSize} onRadioChange={handleChange}>
+          Package Size
+        </RadioButton>
+
+        <TextInput
+          name="contactEmail"
+          value={product.contactEmail}
+          onTextInputChange={handleChange}
+          placeholder="Add your email …"
+        >
+          Contact Email
+        </TextInput>
+
+        <div>
+          <button>Add Product</button>
+          {/* Optional */}
+          <button
+            type="reset"
+            onClick={() => {
+              setProduct(initialProduct);
+              setHasFormErrors(false);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </Form>
+    </section>
+  );
+}
+
+const InputRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  background: #620202;
+  padding: 0.7rem 0.5rem 1.2rem;
+  border-radius: 10px;
+
+  label {
+    display: block;
+    font-weight: bold;
+  }
+  input,
+  select {
+    padding: 0.25rem;
+    margin: 0.5rem 0 1rem;
+  }
+
+  button {
+    background: var(--button-bg);
+    border: 2px solid var(--button-bg);
+    border-radius: 5px;
+    color: var(--primary-color);
+    font-size: 1.2rem;
+    font-weight: 600;
+    padding: 0.5rem;
+    width: 49%;
+  }
+  button:first-child {
+    margin-right: 2%;
+  }
+  button:nth-child(even) {
+    background: transparent;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  align-items: center;
+  background: var(--warning);
+  border-radius: 6px;
+  color: white;
+  display: flex;
+  gap: 2.5rem;
+  margin: 0 0 1rem;
+  padding: 0.5rem;
+
+  div {
+    font-size: 2.5rem;
+    display: inline-block;
+    position: relative;
+    transition: all 0.5s;
+  }
+  div:hover {
+    transform: rotateZ(20deg);
+  }
+
+  div.bubble {
+    font-size: 3rem;
+
+    position: absolute;
+    top: -17px;
+    right: -38px;
+  }
+`;
